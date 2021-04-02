e3f15910721ec83729615976b7f78450f58c3945
LuciaJaramillo <luciaenriquez98@gmail.com>
2021-04-01 22:33:36 -0500

    01-04
    tabla de ranking en docentes

---
+++ ConexionOVA/listapuntajesU2.php
@@ -0,0 +1,12 @@
+<?php
+	header('Access-Control-Allow-Origin: *');
+	include("conexion.php");
+		$sql="SELECT Username,puntos FROM estudiante WHERE id_rol=1 order BY puntos desc LIMIT 10,20";
+		$req=mysqli_query($con,$sql);
+		while ($row = mysqli_fetch_assoc($req)) {
+        echo $row["Username"];
+        //echo $row["puntos"];
+        echo "\n";
+        echo "\n";
+    	}
+?>
