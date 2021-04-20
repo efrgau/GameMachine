d8413b0296c31747dc5d2d5ecdc219a98b56828a
LuciaJaramillo <luciaenriquez98@gmail.com>
2021-04-20 01:05:05 -0500

    20

--- ConexionOVA/conexion.php
+++ ConexionOVA/conexion.php
@@ -1,6 +1,6 @@
 <?php
 	header('Access-Control-Allow-Origin: *');
-	$con=mysqli_connect("localhost","root","","bdgamemachine");
+	$con=mysqli_connect("92.205.11.109","udz0vq5z5rql","Fer-Lusy2","bdgamemachine");
 	//if(mysqli_connect_errno())
 	//{
 	//	echo "Error en la conexion a MYSQL:". mysqli_connect_error();
